e52f766435a628d24ec7961dfd3643f359ac0ee8
PS46355 <texynfy@gmail.com>
2026-02-25 10:53:05 +0700

    Reformat Startup.cs for consistency

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using inventory.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -53,3 +53,4 @@ namespace inventory
         }
     }
 }
+
